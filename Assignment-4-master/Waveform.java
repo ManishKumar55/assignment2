c30e689bbd50a0591880316f00b943304d2fbcc5
ManishKumar55 <manish.ldh55@gmail.com>
2018-12-28 01:19:27 +0530

    first commit

---
+++ Assignment-4-master/Waveform.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Waveform{
+	
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[10];
+		System.out.println("Enter the 10 elements in array: ");
+		for(int i=0;i<10;i++){
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<10;i++){
+			if(i%2==0){
+				System.out.print(a[i+1]+" "+a[i]+" ");
+			}
+		}
+		
+	}
+}
+		
+
+
+
+
+
+
+
+
+
+
+	
+
+
